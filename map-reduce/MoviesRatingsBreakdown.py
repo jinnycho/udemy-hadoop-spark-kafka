fe4e6596d3fd4271f046167cb1dd6685b874c267
jinnycho <jinnycho.eng@gmail.com>
2019-06-09 20:21:53 -0400

    Print movies in order of count

--- map-reduce/MoviesRatingsBreakdown.py
+++ map-reduce/MoviesRatingsBreakdown.py
@@ -14,7 +14,29 @@ class MoviesRatingsBreakdown(MRJob):
         yield movieID, 1
 
     def reducer_count_ratings(self, key, values):
-        yield key, count(values)
+        yield key, sum(values)
+
+# Sort the movies by their numbers of ratings
+class MoviesRatingsBreakdownSort(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_ratings,
+                   reducer=self.reducer_count_ratings),
+            MRStep(reducer=self.sort_count_ratings)
+        ]
+
+    def mapper_get_ratings(self, _, line):
+        (userID, movieID, rating, timestamp) = line.split('\t')
+        yield movieID, 1
+
+    def reducer_count_ratings(self, key, values):
+        # make ratings_count as key so we can sort them by key
+        # + movies all concatenated for the same num of ratings_count
+        yield str(sum(values)).zfill(5), key
+
+    def reducer_count_ratings(self, key, values):
+        yield key, sum(values)
 
 if __name__ == '__main__':
-    MoviesRatingsBreakdown.run()
+    #MoviesRatingsBreakdown.run()
+    MoviesRatingsBreakdownSort.run()
