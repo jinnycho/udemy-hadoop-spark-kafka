403567a59e13f3486549b6a01bc8349027707d04
jinnycho <jinnycho.eng@gmail.com>
2020-01-02 23:08:55 -0500

    Added Configs

--- kafka-start/src/main/java/com/github/jinnycho503/kafka/tutorial/ProducerDemo.java
+++ kafka-start/src/main/java/com/github/jinnycho503/kafka/tutorial/ProducerDemo.java
@@ -1,7 +1,21 @@
 package com.github.jinnycho503.kafka.tutorial;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        String bootstrapServersAddr = "127.0.0.1:9092";
+        // create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServersAddr);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSe rializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+
+        // send data
     }
 }
