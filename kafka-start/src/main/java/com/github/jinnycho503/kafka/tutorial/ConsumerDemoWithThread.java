dd0568337cf7e191264f8b04a89507b8012f9376
jinnycho <jinnycho.eng@gmail.com>
2020-01-05 21:46:16 -0500

    Working Threading

--- kafka-start/src/main/java/com/github/jinnycho503/kafka/tutorial/ConsumerDemoWithThread.java
+++ kafka-start/src/main/java/com/github/jinnycho503/kafka/tutorial/ConsumerDemoWithThread.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,50 +16,95 @@ import java.util.concurrent.CountDownLatch;
 
 public class ConsumerDemoWithThread {
     public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger(ConsumerDemoWithThread.class.getName());
+        new ConsumerDemoWithThread().run();
+    }
 
+    private ConsumerDemoWithThread() {
+
+    }
+
+    private void run() {
+        Logger logger = LoggerFactory.getLogger(ConsumerDemoWithThread.class.getName());
+        String topic1 = "first_topic";
         String bootstrapServers = "127.0.0.1:9092";
         String groupID = "my-third-application";
         String offset_reset_config = "earliest";
-        String topic1 = "first_topic";
+        CountDownLatch latch = new CountDownLatch(1);
 
-        Properties properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        // Take bytes and convert them to string
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupID);
-        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, offset_reset_config); // option: earliest, latest, none
+        // Create the consumer runnable
+        logger.info("Creating the consumer thread.");
+        Runnable myConsumerRunnable = new ConsumerRunnable(bootstrapServers, groupID, offset_reset_config, topic1, latch);
 
-        // create consumer
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+        // Start the thread
+        Thread myThread = new Thread(myConsumerRunnable);
+        myThread.start();
 
-        // subscribe consumer to topic(s)
-        consumer.subscribe(Arrays.asList(topic1));
-
-        // poll new data
-        while (true) {
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-            for (ConsumerRecord<String, String> record : records) {
-                logger.info("Key: " + record.key() + "\n" + "Value: " + record.value() + "\n" + "Partition: " + record.partition() + "\n" + "Offset: " + record.offset());
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("Caught shutdown hook");
+            ((ConsumerRunnable) myConsumerRunnable).shutdown();
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+            logger.info("Application has exited");
+        }));
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            logger.error("Application got interrupted", e);
+        } finally {
+            logger.info("Application is closing.");
         }
     }
 
-    public class ConsumerThread implements Runnable {
+    public class ConsumerRunnable implements Runnable {
+
+        private CountDownLatch latch;
+        private KafkaConsumer<String, String> consumer;
+        private Logger logger = LoggerFactory.getLogger(ConsumerRunnable.class.getName());
 
         // CountDownLatch: to deal with concurrency
-        public ConsumerThread(CountDownLatch latch) {
+        public ConsumerRunnable(String bootstrapServers, String groupID, String offset_reset_config, String topic, CountDownLatch latch) {
+            this.latch = latch;
 
+            Properties properties = new Properties();
+            properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+            // Take bytes and convert them to string
+            properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+            properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+            properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupID);
+            properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, offset_reset_config); // option: earliest, latest, none
+            // create consumer
+            consumer = new KafkaConsumer<String, String>(properties);
+
+            // subscribe consumer to topic(s)
+            consumer.subscribe(Arrays.asList(topic));
         }
 
         @Override
         public void run() {
-
+            try {
+            // poll new data
+                while (true) {
+                    ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                    for (ConsumerRecord<String, String> record : records) {
+                        logger.info("Key: " + record.key() + "\n" + "Value: " + record.value() + "\n" + "Partition: " + record.partition() + "\n" + "Offset: " + record.offset());
+                    }
+                }
+            } catch (WakeupException e) {
+                logger.info("Received shutdown signal.");
+            } finally {
+                consumer.close();
+                // tell main code that we're done with the consumer
+                latch.countDown();
+            }
         }
 
         public void shutdown() {
-
+            // wakeup(): special method to interrupt consumer.poll()
+            // will throw WakeUpException
+            consumer.wakeup();
         }
     }
 }
