1f3488a35bce062a80d001a3d25fbdcdf0f652e0
jinnycho <jinnycho.eng@gmail.com>
2020-01-14 00:48:58 -0500

    Write a basic function: createKafkaProducer()

--- kafka-start/src/main/java/com/github/jinnycho503/kafka/twitter/ElasticSearchConsumer.java
+++ kafka-start/src/main/java/com/github/jinnycho503/kafka/twitter/ElasticSearchConsumer.java
@@ -1,4 +1,9 @@
 package com.github.jinnycho503.kafka.twitter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ElasticSearchConsumer {
+    public static void main(String[] args) {
+    }
 }
--- kafka-start/src/main/java/com/github/jinnycho503/kafka/twitter/TwitterProducer.java
+++ kafka-start/src/main/java/com/github/jinnycho503/kafka/twitter/TwitterProducer.java
@@ -1,4 +1,30 @@
 package com.github.jinnycho503.kafka.twitter;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
 public class TwitterProducer {
+    public static void main(String[] args) {
+
+    }
+
+    public KafkaProducer<String, String> createKafkaProducer() {
+        final Logger logger = LoggerFactory.getLogger(TwitterProducer.class);
+
+        // Producer Properties
+        String bootstrapServerAddr = "127.0.0.1:9092";
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServerAddr);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+        return producer;
+    }
 }
