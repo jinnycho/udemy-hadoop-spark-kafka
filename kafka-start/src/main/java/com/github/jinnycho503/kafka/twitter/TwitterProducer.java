063226ce6d3b0f91238f3b24bc5069593f60b536
jinnycho <jinnycho.eng@gmail.com>
2020-01-14 01:13:58 -0500

    Set up basic Client and Kafka producer

--- kafka-start/src/main/java/com/github/jinnycho503/kafka/twitter/TwitterProducer.java
+++ kafka-start/src/main/java/com/github/jinnycho503/kafka/twitter/TwitterProducer.java
@@ -1,14 +1,25 @@
 package com.github.jinnycho503.kafka.twitter;
 
+import com.google.common.collect.Lists;
+import com.twitter.hbc.ClientBuilder;
 import com.twitter.hbc.core.Client;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.Hosts;
+import com.twitter.hbc.core.HttpHosts;
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class TwitterProducer {
     public static void main(String[] args) {
@@ -16,10 +27,35 @@ public class TwitterProducer {
     }
 
     public void run() {
+        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(100);
+        KafkaProducer<String, String> kafkaProducer = createKafkaProducer();
+        Client twitterClient = createTwitterClient(msgQueue);
+        twitterClient.connect();
+
     }
 
     public Client createTwitterClient(BlockingQueue<String> msgQueue) {
+        // Declare the connection information
+        Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
+        StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
+        List<String> terms = Lists.newArrayList("dog", "cat");
+        hosebirdEndpoint.trackTerms(terms);
 
+        String consumerKey = "dS4U9f2EbmsctsIp1LGsYhPHN";
+        String consumerSecret = "A9uhXRWiMBRUvnM6KmmZgkJp6fse2d5tgJVFy7qIttDipfwfV5";
+        String token = "3004406534-iQC8JDHeePjUe3A4vu1glyMa7lVxXViDWm19cEc";
+        String secret = "U4Gcy4tKR08NJcPvSfL2mM0agCBTYLFgihIO3f4ZzJ10z";
+        Authentication hosebirdAuth = new OAuth1(consumerKey, consumerSecret, token, secret);
+
+        ClientBuilder builder = new ClientBuilder()
+                .name("Hosebird-client-01")
+                .hosts(hosebirdHosts)
+                .authentication(hosebirdAuth)
+                .endpoint(hosebirdEndpoint)
+                .processor(new StringDelimitedProcessor(msgQueue));
+
+        Client hosebirdClient = builder.build();
+        return hosebirdClient;
     }
 
     public KafkaProducer<String, String> createKafkaProducer() {
